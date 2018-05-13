5223cee4231b792538d0809f183364f48fdbd268
QuandisS <anton.kravtsov.31@gmail.com>
2018-05-13 23:30:04 +0300

    add localizations file (for future lang. updates)

---
+++ localizations.py
@@ -0,0 +1,12 @@
+class Localization():
+    def __init__(self, main_window_title, show_vars_title):
+        self.main_window_title = main_window_title
+        self.show_vars_title = show_vars_title
+
+english = Localization('TTTH Physics',
+                       'Enter a data')
+
+
+
+russian = Localization('TTH Physics',
+                       'Введите данные')
